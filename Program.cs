745cd0b924b236798092b32716cc4ca3b102b172
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-08-06 20:07:30 -0400

    Modulus & Variable math practice

--- Program.cs
+++ Program.cs
@@ -2,11 +2,38 @@
 
 namespace csharp
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(5 % 2); //Modulus
+            //Console.WriteLine(5 / 2.0); //Division
+
+            Console.WriteLine();
+
+            int numPlus = 12;
+            numPlus++; //adds 1 to the number
+            Console.WriteLine($"Num++ {numPlus}");
+
+            int num = 12;
+            Console.WriteLine($"Number: {num}");
+
+            int numMinus = 12;
+            numMinus--; //subtracts 1 from the number
+            Console.WriteLine($"Num-- {numMinus}");
+
+
+
+
+        }
+
+
+
+
+
+
+
     }
-  }
+
 }
+
